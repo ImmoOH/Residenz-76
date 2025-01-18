1da011dc1054efda6f01d776b5f1907f9e059ea9
ImmoOH <r.muentz@web.de>
2025-01-18 07:06:36 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,308 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-eingangsbereich",
+      "name": "Eingangsbereich",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4539844658315211,
+        "pitch": 0.01791626809338709,
+        "fov": 1.515352868188304
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5058397090988329,
+          "pitch": 0.011892126119805724,
+          "rotation": 0,
+          "target": "1-hausflur"
+        },
+        {
+          "yaw": 1.5244916928037977,
+          "pitch": 0.2019455790541933,
+          "rotation": 0,
+          "target": "2-duschbad"
+        },
+        {
+          "yaw": -2.9090688779713965,
+          "pitch": 0.027793755773220852,
+          "rotation": 0,
+          "target": "3-flur"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hausflur",
+      "name": "Hausflur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.0992828657055806,
+        "pitch": 0.04372660017657459,
+        "fov": 1.515352868188304
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6626257287235742,
+          "pitch": 0.04275334960016863,
+          "rotation": 0,
+          "target": "0-eingangsbereich"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-duschbad",
+      "name": "Duschbad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.4269915094575154,
+        "pitch": 0.1470305653030941,
+        "fov": 1.515352868188304
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.34393826378120806,
+          "pitch": 0.1524309000348545,
+          "rotation": 0,
+          "target": "0-eingangsbereich"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-flur",
+      "name": "Flur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.8388302901838376,
+        "pitch": 0.07237506236123181,
+        "fov": 1.515352868188304
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7360809216323965,
+          "pitch": 0.20429396482184536,
+          "rotation": 0,
+          "target": "6-zimmer-2"
+        },
+        {
+          "yaw": -0.772422181675001,
+          "pitch": 0.1380859463941313,
+          "rotation": 0,
+          "target": "5-zimmer-1"
+        },
+        {
+          "yaw": 2.9915625437846716,
+          "pitch": 0.29964462932467484,
+          "rotation": 0,
+          "target": "4-wohn--essbereich-mit-kche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-wohn--essbereich-mit-kche",
+      "name": "Wohn-/ Essbereich mit Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.1475107109476212,
+        "pitch": 0.004497156397567537,
+        "fov": 1.515352868188304
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4199801084483994,
+          "pitch": 0.11093884560907341,
+          "rotation": 0,
+          "target": "3-flur"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.558251737949897,
+          "pitch": 0.09043463252283601,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Zugang zum Balkon</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Von hier aus haben Sie einen Blick auf die Ostsee</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "5-zimmer-1",
+      "name": "Zimmer 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.6988905396029477,
+        "pitch": 0.11007040734104301,
+        "fov": 1.515352868188304
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9970927094478714,
+          "pitch": 0.1729307853322517,
+          "rotation": 0,
+          "target": "3-flur"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-zimmer-2",
+      "name": "Zimmer 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.6818166664856005,
+        "pitch": 0.07388287616044309,
+        "fov": 1.515352868188304
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5939362935334778,
+          "pitch": 0.17164490264828558,
+          "rotation": 0,
+          "target": "3-flur"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Residenz 76",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
